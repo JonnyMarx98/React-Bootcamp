a39f726d227c79bb4ef39724a8e7a6d7f0dfb7f7
Jonny <jonnymarx98@gmail.com>
2019-09-14 10:00:03 +0100

    Updates

---
+++ precache-manifest.b2228629c46c62a99eb24fc998e27c71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ba4680ebe1d134beeef8d08d9da237",
+    "url": "/React-Bootcamp/index.html"
+  },
+  {
+    "revision": "de568a6cccf6fedddd92",
+    "url": "/React-Bootcamp/static/css/main.23d849b4.chunk.css"
+  },
+  {
+    "revision": "b15f9b36f2a206ce6b69",
+    "url": "/React-Bootcamp/static/js/2.b54649ab.chunk.js"
+  },
+  {
+    "revision": "de568a6cccf6fedddd92",
+    "url": "/React-Bootcamp/static/js/main.57d16ee4.chunk.js"
+  },
+  {
+    "revision": "e8aafd6744f788267595",
+    "url": "/React-Bootcamp/static/js/runtime~main.42256f52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Bootcamp/static/media/logo.5d5d9eef.svg"
+  }
+]);
