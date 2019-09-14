ecdb62c86398a13284de76e52da11f26cfed4893
Jonny <jonnymarx98@gmail.com>
2019-09-14 11:17:14 +0100

    Pokemon app finished

--- pokemon-app/src/Pokecard.js
+++ pokemon-app/src/Pokecard.js
@@ -4,22 +4,27 @@ import './Pokecard.css';
 const POKE_API = 
 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
+const POKE_API_2 = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+const POKE_WIKI = 'https://pokemon.fandom.com/wiki/'
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3):number);
+
 class Pokecard extends Component {
-    static defaultProps = {
-        id: 4, 
-        name: 'Charmander', 
-        type: 'fire', 
-        base_experience: 62
-    }
     render() {
         const props = this.props;
-        let imgSrc = `${POKE_API}${props.id}.png`
+        let imgSrc = `${POKE_API_2}${padToThree(props.id)}.png`;
+        let wikiLink = `${POKE_WIKI}${props.name}`;
         return (
             <div className="Pokecard">
-                <h1>{props.name}</h1>
-                <img src={imgSrc} alt={props.name}></img>
-                <div>{props.type}</div>
-                <div>{props.base_experience}</div>
+                <h1 className="Pokecard-title">{props.name}</h1>
+                <a href={wikiLink}>
+                    <div className="Pokecard-img">
+                        <img src={imgSrc} alt={props.name}></img>
+                    </div>
+                </a>
+                
+                <div className="Pokecard-data">{props.type}</div>
+                <div className="Pokecard-data">{props.base_experience}</div>
             </div>
         )
     } 
