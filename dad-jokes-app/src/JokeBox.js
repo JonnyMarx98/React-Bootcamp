5aee03e2ce7aa297fa7e272f3f3aed578a6d7f6e
Jonny <jonnymarx98@gmail.com>
2019-11-02 13:56:38 +0000

    some css for jokes app

--- dad-jokes-app/src/JokeBox.js
+++ dad-jokes-app/src/JokeBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Joke from './Joke';
 import axios from 'axios';
+import './JokeBox.css';
 
 class JokeBox extends Component {
     static defaultProps = {
@@ -48,12 +49,21 @@ class JokeBox extends Component {
             <li>{joke}</li>
         ))
         return(
-            <div>
-                <h1>Dad Jokes 🤣</h1>
-                <button>New Jokes</button>
-                <ul>
-                    {jokes}
-                </ul> 
+            <div className='JokeBox'>
+                <div className='JokeBox-sidebar'>
+                    <h1 className='JokeBox-title'>
+                        <span>Dad</span> Jokes 🤣
+                    </h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg'></img>
+                </div>
+                
+                {/* <button>New Jokes</button> */}
+                <div className='JokeBox-jokes'>
+                    <ul>
+                        {jokes}
+                    </ul> 
+                </div>
+                
             </div>
         )
     }
