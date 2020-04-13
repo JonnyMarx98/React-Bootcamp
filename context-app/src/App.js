539f1ecce5cc434a7cd503bf276b12066fc2ac50
Jonny <jonnymarx98@gmail.com>
2020-04-13 09:57:04 +0100

    added form in

--- context-app/src/App.js
+++ context-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import Navbar from "./Navbar";
+import Form from './Form';
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Form />
+    </>    
   );
 }
 
