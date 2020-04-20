e3df86f25155f3d85d0a6a357a3cd7f5e8842863
Jonny <jonnymarx98@gmail.com>
2020-04-20 08:54:29 +0100

    refactored using a reducer

--- todos-context-usereducer/src/contexts/todos.context.js
+++ todos-context-usereducer/src/contexts/todos.context.js
@@ -1,6 +1,7 @@
 // todos
 // todos methods
-import React, {createContext} from 'react';
+import React, {createContext, useReducer} from 'react';
+import todoReducer from '../reducers/todo.reducer';
 import useTodoState from '../hooks/useTodoState';
 
 const defaultTodos = [
@@ -10,9 +11,10 @@ const defaultTodos = [
 export const TodosContext = createContext();
 
 export function TodosProvider(props){
-  const todosStuff = useTodoState(defaultTodos);
+  // const todosStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return(
-    <TodosContext.Provider value={todosStuff}>
+    <TodosContext.Provider value={{todos, dispatch}}>
       {props.children}
     </TodosContext.Provider>
   )
